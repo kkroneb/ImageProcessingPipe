b3f07115cbc6667869016650b8caaece0c329bfb
karl.kroneberg <karl.kroneberg@s2021.tu-chemnitz.de>
2023-06-13 14:29:19 +0100

    add Images, write header file

---
+++ include/ProcessImage.h
@@ -0,0 +1,35 @@
+// Include Guard: Verhindert mehrfache Einbindung der Header-Datei
+#ifndef PROCESSIMAGE_H
+#define PROCESSIMAGE_H
+
+// Libraries
+#include <opencv2/opencv.hpp>  
+#include <boost/filesystem.hpp>
+
+
+/// FUNCTIONS ///
+
+// Controller-Function: processes all images in given directory
+void processImagesInDirectory(const boost::filesystem::path& directory);
+
+// read image and return as a matrix
+cv::Mat readImage(const boost::filesystem::path& imagePath);
+
+// enhance contrast
+cv::Mat enhanceContrast(const cv::Mat& image);
+
+// reduce noise
+cv::Mat reduceNoise(const cv::Mat& image);
+
+// highlight edges
+cv::Mat highlightEdges(const cv::Mat& image);
+
+// color quantization (reducing the number of colors)
+cv::Mat quantizeColors(const cv::Mat& image);
+
+// save file
+void saveImage(const cv::Mat& image, const boost::filesystem::path& outputPath);
+
+
+// end of include guard
+#endif //PROCESSIMAGE_H
